8e7074827f49c4ac8f8276b403c546af472296fe
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2023-12-20 12:22:32 +1100

    fix: Improve usage of mock namespace flag

--- k8s-deployer/scripts/k8s-manage-namespace.sh
+++ k8s-deployer/scripts/k8s-manage-namespace.sh
@@ -59,20 +59,26 @@ Missing namespace paramter. Usage:
   exit 1
 fi
 
+echo "PARENT_NS=${PARENT_NS}"
+echo "ACTION=${ACTION}"
+echo "NS=${NS}"
+echo "TIMEOUT_SECONDS=${TIMEOUT_SECONDS}"
+
 if [ "${ACTION}" == "create" ];
 then
+
+  if [ "${MOCK_NS}" != "" ];
+  then
+    echo "Faking ns creation...."
+    echo "$STATUS_DONE"
+    exit 0
+  fi
+
   existsAlready=$(kubectl get ns ${NS} --ignore-not-found)
   if [ "${existsAlready}" == "" ];
   then
     echo "Creating namespace: ${NS} under parent ${PARENT_NS}, this may take some time..."
     
-    if [ "${MOCK_NS}" != "" ];
-    then
-      echo "Faking ns creation...."
-      echo "$STATUS_DONE"
-      exit 0
-    fi
-
     kubectl hns create $NS -n $PARENT_NS  
     returnStatus=$(($?+0))
 
