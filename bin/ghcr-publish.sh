4353696290a6ab728b517bdc84b6673dd1684b53
Amninder Kaur <amninder.kaur@kindredgroup.com>
2024-01-12 12:42:24 +1100

    fix: the version of the image tag

--- bin/ghcr-publish.sh
+++ bin/ghcr-publish.sh
@@ -5,7 +5,7 @@ package_name=$1
 version=$2
 
 docker_file=$package_name/Dockerfile
-tag_with_version=pit-$package_name:v$version
+tag_with_version=pit-$package_name:$version
 tag_ref=ghcr.io/kindredgroup/pit-toolkit/$tag_with_version
 echo $tag_ref
 docker build -f $docker_file . --tag $tag_ref
