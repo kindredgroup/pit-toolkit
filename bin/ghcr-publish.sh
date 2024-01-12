fa4b026b32395ba182184fbe85d5af7950488390
Amninder Kaur <amninder.kaur@kindredgroup.com>
2024-01-12 13:08:17 +1100

    chore: better approach

--- bin/ghcr-publish.sh
+++ bin/ghcr-publish.sh
@@ -3,11 +3,11 @@
 set -e
 package_name=$1
 version=$2
-
-docker_file=$package_name/Dockerfile
+echo $(pwd)
+cd $package_name
 tag_with_version=pit-$package_name:$version
 tag_ref=ghcr.io/kindredgroup/pit-toolkit/$tag_with_version
 echo $tag_ref
-docker build -f $docker_file . --tag $tag_ref
+docker build . --tag $tag_ref
 #for ghcr.io access token mentioned in the github secrets and accessed in actions
 docker push $tag_ref
