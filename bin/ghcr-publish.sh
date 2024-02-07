42e6532e443820b3eebe229d559a9d728ed83ad8
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2024-02-07 15:39:00 +1100

    fix: Remove pit- suffix from docker image image name

--- bin/ghcr-publish.sh
+++ bin/ghcr-publish.sh
@@ -5,7 +5,7 @@ package_name=$1
 version=$2
 
 cd $package_name
-tag_with_version=pit-$package_name:$version
+tag_with_version=$package_name:$version
 tag_ref=ghcr.io/kindredgroup/pit-toolkit/$tag_with_version
 echo $tag_ref
 docker build . --tag $tag_ref
