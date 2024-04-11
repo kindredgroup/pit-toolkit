ab39253b147c02ed2cf481723ca7e9f19a6e320e
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2024-04-12 09:53:06 +1000

    Implement wait until done

--- examples/load-generator/src/index.ts
+++ examples/load-generator/src/index.ts
@@ -12,6 +12,7 @@ declare type ItemType = string
 const main = async () => {
   const rateTps = parseInt(process.argv[2])
   const durationSeconds = parseInt(process.argv[3])
+  const slowProcessingDelay = parseInt(process.argv[4])
 
   logger.info("Load generator will run for %ss at the rate of %stps. The approx stop time is %s", durationSeconds, rateTps, new Date(new Date().getTime() + durationSeconds * 1000))
 
@@ -20,21 +21,29 @@ const main = async () => {
   for await (const task of generateAtRate(rateTps, durationSeconds, fnGenerateNewItem)) {
     pool.submit(async () => {
       histLoadRate.recordValue(Date.now())
-      await business.logItemAndSleep(0, task)
+      await business.logItemAndSleep(slowProcessingDelay, task)
     })
   }
 
-  const elapsedMs = (histLoadRate.maxValue - histLoadRate.minNonZeroValue)
-  logger.info(`
-    Load Generator Metrics:
-    --------------------------------------------
-    count         : %s
-    effective rate: %s (tps)
-    duration      : %s (sec)`,
-    histLoadRate.totalCount,
-    Math.round(histLoadRate.totalCount / elapsedMs * 1000 * 100) / 100,
-    Math.round(elapsedMs / 10) / 100
-  )
+  // await until done
+  setTimeout(async () => {
+    while (histLoadRate.totalCount < rateTps * durationSeconds) {
+      logger.info("Processed: %s", histLoadRate.totalCount)
+      await (new Promise(resolve => setTimeout(resolve, 1000)))
+    }
+    const elapsedMs = (histLoadRate.maxValue - histLoadRate.minNonZeroValue)
+    logger.info(`
+      Load Generator Metrics:
+      --------------------------------------------
+      count         : %s
+      effective rate: %s (tps)
+      duration      : %s (sec)`,
+      histLoadRate.totalCount,
+      Math.round(histLoadRate.totalCount / elapsedMs * 1000 * 100) / 100,
+      Math.round(elapsedMs / 10) / 100
+    )
+  }, 0 )
+
 }
 
 const fnGenerateNewItem = (i: string): ItemType => {
