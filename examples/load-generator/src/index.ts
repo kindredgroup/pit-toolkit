b0084b072aaea972284b8d2cc9ff95efb89c4742
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2024-04-12 14:27:48 +1000

    Add more samples

--- examples/load-generator/src/index.ts
+++ examples/load-generator/src/index.ts
@@ -17,33 +17,56 @@ const main = async () => {
   logger.info("Load generator will run for %ss at the rate of %stps. The approx stop time is %s", durationSeconds, rateTps, new Date(new Date().getTime() + durationSeconds * 1000))
 
   const pool = new Pond(CONCURRENCY)
+  const histBusinessMetrics = hdr.build();
+  const histBusinessMetricsTps = hdr.build();
   const histLoadRate = hdr.build();
   for await (const task of generateAtRate(rateTps, durationSeconds, fnGenerateNewItem)) {
     pool.submit(async () => {
       histLoadRate.recordValue(Date.now())
-      await business.logItemAndSleep(slowProcessingDelay, task)
+      const startedAt = Date.now()
+      await business.logItemAndSleep(Math.random() * (slowProcessingDelay - 1) + 1, task)
+      histBusinessMetrics.recordValue(Date.now() - startedAt)
+      histBusinessMetricsTps.recordValue(Date.now())
     })
   }
 
-  // await until done
   setTimeout(async () => {
-    while (histLoadRate.totalCount < rateTps * durationSeconds) {
-      logger.info("Processed: %s", histLoadRate.totalCount)
+    while (histBusinessMetricsTps.totalCount < rateTps * durationSeconds) {
+      logger.info("progress ... processed: %s of %s", histBusinessMetricsTps.totalCount, rateTps * durationSeconds)
       await (new Promise(resolve => setTimeout(resolve, 1000)))
     }
-    const elapsedMs = (histLoadRate.maxValue - histLoadRate.minNonZeroValue)
-    logger.info(`
-      Load Generator Metrics:
-      --------------------------------------------
-      count         : %s
-      effective rate: %s (tps)
-      duration      : %s (sec)`,
-      histLoadRate.totalCount,
-      Math.round(histLoadRate.totalCount / elapsedMs * 1000 * 100) / 100,
-      Math.round(elapsedMs / 10) / 100
-    )
-  }, 0 )
 
+    logger.info("progress ... processed: %s of %s", histBusinessMetricsTps.totalCount, rateTps * durationSeconds)
+
+    await printReport("Load Generator", histLoadRate, rateTps, durationSeconds)
+    await printReport("Buisness Service Metrics", histBusinessMetricsTps, rateTps, durationSeconds)
+
+    logger.info("\n\n")
+    logger.info(`
+
+        Buisness Service Metrics (details):
+        --------------------------------------------
+        count: %s
+        min  : %s (ms)
+        max  : %s (ms)
+
+        p50  : %s (ms)
+        p75  : %s (ms)
+        p90  : %s (ms)
+        p95  : %s (ms)
+        p100 : %s (ms)
+
+      `,
+      histBusinessMetrics.totalCount,
+      histBusinessMetrics.minNonZeroValue,
+      histBusinessMetrics.maxValue,
+      histBusinessMetrics.getValueAtPercentile(50),
+      histBusinessMetrics.getValueAtPercentile(75),
+      histBusinessMetrics.getValueAtPercentile(90),
+      histBusinessMetrics.getValueAtPercentile(95),
+      histBusinessMetrics.getValueAtPercentile(100)
+    )
+  })
 }
 
 const fnGenerateNewItem = (i: string): ItemType => {
@@ -52,6 +75,23 @@ const fnGenerateNewItem = (i: string): ItemType => {
   return `item-${ i } [${ time }]`
 }
 
+const printReport = async (name: string, hist: hdr.Histogram, rateTps: number, durationSeconds: number) => {
+  // await until done
+  const elapsedMs = (hist.maxValue - hist.minNonZeroValue)
+  logger.info(`
+
+    %s Metrics:
+    --------------------------------------------
+    count         : %s
+    effective rate: %s (tps)
+    duration      : %s (sec)`,
+
+    name,
+    hist.totalCount,
+    Math.round(hist.totalCount / elapsedMs * 1000 * 100) / 100,
+    Math.round(elapsedMs / 10) / 100
+  )
+}
 
 main()
   .catch(e => {
