0e4d436bd61fbad5fc4c036bde6e2e0685a017a5
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2024-04-23 13:35:31 +1000

    feat: Implement NS cleanup

--- brownie/scripts/clean-ns.sh
+++ brownie/scripts/clean-ns.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# This script is invoked from the periodic CI pipeline. It will fetch the list of children
+# namespaces the kubernetes and pass that list into the node app. The node app will determine
+# whether the namespace is old and should be removed or it hasn't been aged yet and should be
+# kept in the cluster. If the "--dry-run" parameter is "false" then old namespaces will be removed.
+# The actual removeal of namespace is delegeated to
+# ../k8s-deployer/scripts/k8s-manage-namespace.sh
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 K8S_NAMESPACE=$1
+BROWNIE_TIMEOUT=$2
+DRY_RUN=$3
 
-kubectl get ns -l "${K8S_NAMESPACE}.tree.hnc.x-k8s.io/depth=1" -ojson | jq '.items' > /tmp/ns-list.json
+if [ "${DRY_RUN}" == "" ]; then DRY_RUN="true"; fi
 
-node dist/src/k8ns/index.js /tmp/ns-list.json 1minute
+usage="Example: $0 my-parent-namespace 3days"
+if [ "${K8S_NAMESPACE}" == "" ];
+then
+  echo "Missing 1st parameter namespace"
+  echo $usage
+  exit 1
+fi
+
+if [ "${BROWNIE_TIMEOUT}" == "" ];
+then
+  echo "Missing 2nd parameter namespace retention period"
+  echo $usage
+  exit 1
+fi
+
+LOG_FILE="./brownie-ns-list.tmp.json"
+
+kubectl get ns -l "${K8S_NAMESPACE}.tree.hnc.x-k8s.io/depth=1" -ojson | jq '.items' > $LOG_FILE
+
+node dist/src/k8ns/index.js --dry-run $DRY_RUN --ns-file $LOG_FILE --retention-period $BROWNIE_TIMEOUT
--- brownie/src/k8ns/index.ts
+++ brownie/src/k8ns/index.ts
@@ -1,28 +1,26 @@
-import * as fs from "fs"
 import * as Shell from "node:child_process"
 
 import { logger } from "../logger.js"
-import { Config } from "../config.js"
+import * as Core from "./core.js"
 
 const main = async () => {
-  const nsDataFile = process.argv[2]
-  const retentionRaw = process.argv[3]
+  const params = new Map<string, string>()
 
-  // validate ...
-
-  const nsListRaw = fs.readFileSync(`${ nsDataFile }`).toString("utf-8")
-  let nsList = null
-  try {
-    nsList = JSON.parse(nsListRaw)
-  } catch (e) {
-    logger.warn("\n%s", nsListRaw)
-    throw new Error(`Unable to parse raw list of namespaces in '${ nsDataFile }'`, { cause: e })
+  let deletedCount = 0
+  for (let i = 2; i < process.argv.length; i+=2) {
+    params.set(process.argv[i], process.argv[i + 1])
+  }
+  const config = Core.loadConfig(params)
+  if (config.nsList.length == 0) {
+    logger.debug("There are no namespaces to delete")
+    return
   }
 
-  logger.info("\n%s", JSON.stringify(nsList, null, 2))
+  logger.info("Analysing the list of %s namespaces", config.nsList.length)
 
-  const retentionMinutes = Config.parseRetention(retentionRaw)
-  for (let ns of nsList) {
+  for (let ns of config.nsList) {
+    logger.info("")
+
     if (!ns.metadata.creationTimestamp) {
       logger.info("Namespace '%s' does not have 'creationTimestamp' information. Skipping...", ns.metadata.name)
       continue
@@ -30,54 +28,39 @@ const main = async () => {
 
     const nsName = ns.metadata.name
     const ageInMinutes = (new Date().getTime() - Date.parse(ns.metadata.creationTimestamp)) / 60_000
-    logger.info("Namespace '%s' was created at: %s. It is %s minutes old", nsName, ns.metadata.creationTimestamp, ageInMinutes)
-    if (ageInMinutes < retentionMinutes) {
-      logger.info("Namespace '%s' hasn't aged enough. Will be cleaned after %s minutes", nsName, (retentionMinutes - ageInMinutes))
+    logger.info("Namespace '%s' was created at: %s. It is %s minutes old", nsName, ns.metadata.creationTimestamp, ageInMinutes.toFixed(2))
+    if (ageInMinutes < config.retentionMinutes) {
+      logger.info("Namespace '%s' hasn't aged enough. Will be cleaned after %s minutes", nsName, (config.retentionMinutes - ageInMinutes).toFixed(2))
       continue
     }
 
-    const parentNsName = ns.metadata.annotations["hnc.x-k8s.io/subnamespace-of"]
+    const parentNsName = ns.metadata.annotations[Core.HNC_PARENT_ANNOTATION]
     if (!parentNsName) {
-      logger.warn("The child namespace '%s', does not have 'hnc.x-k8s.io/subnamespace-of' annotation. It might be misconfigured, hence need to be dealt with manually. Skipping...")
+      logger.warn("The child namespace '%s', does not have '%s' annotation. It might be misconfigured, hence need to be dealt with manually. Skipping...", nsName, Core.HNC_PARENT_ANNOTATION)
       continue
     }
 
+    logger.info("Deleting namespace '%s' under '%s'", nsName, parentNsName)
+    if (config.dryRun) {
 
-    logger.info("Cleaning namespace '%s' under '%s'", nsName, parentNsName)
+      logger.info("Namespace '%s > %s' has NOT been deleted (dry run mode).", parentNsName, nsName)
+      deletedCount++
 
-    const logFile = `./clean-ns-${nsName}.log`
-    const out = fs.openSync(logFile, 'a')
-    const err = fs.openSync(logFile, 'a')
+    } else {
 
-    const fnCleanLogData = (data: any): string => {
-      if (!data) return
-      let text = data.toString()
-      if (text.trim().length == 0) return
-      if (text.endsWith("\n")) text = text.substring(0, text.length-1)
-      return text
-    }
-    let tailer = null
-    setTimeout(() => {
-      tailer = Shell.spawn("tail", [ "-f", logFile ], { detached: true })
-      tailer.stdout.on('data', function(data){
-        const text = fnCleanLogData(data)
-        if (text) logger.info(text)
-      })
-      tailer.stderr.on('data', function(data){
-        const text = fnCleanLogData(data)
-        if (text) logger.error(text)
-      })
-    }, 0)
-    // setTimeout(() => {
-    const process = Shell.spawnSync(
-      "../k8s-deployer/scripts/k8s-manage-namespace.sh",
-      [ parentNsName, "delete", nsName, 120 ],
-      {
-        stdio: [ 'ignore', out, err ]
+      try {
+
+        const script = "../k8s-deployer/scripts/k8s-manage-namespace.sh"
+        const scriptParams = [ parentNsName, "delete", nsName, 120 ]
+        Shell.spawnSync(script, scriptParams, { stdio: [ 'inherit', 'inherit', 'inherit' ] })
+        deletedCount++
+
+      } catch (e) {
+        logger.error("Unable to delete namespace %s > %s. Error: %s", parentNsName, nsName, e.message)
+        if (e.cause) logger.error(e.cause)
+        if (e.stack) logger.error("Stack:\n%s", e.stack)
       }
-    )
-
-    if (tailer) tailer.kill()
+    }
   }
 }
 
