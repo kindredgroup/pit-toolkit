1e9a4c0bc3fc1d3de7bc03c6353f7c625d647e37
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2024-05-02 11:03:38 +1000

    Initial commit

---
+++ examples/consumer-metrics/src/consumer/index.ts
@@ -0,0 +1,13 @@
+import { logger } from "../logger.js"
+
+const main = async () => {
+  logger.info("main(): Starting consumer...")
+}
+
+main()
+  .catch(e => {
+    logger.error("Message: %s", e.message)
+    if (e.cause) logger.error(e.cause)
+    if (e.stack) logger.error("Stack:\n%s", e.stack)
+    process.exit(1)
+  })
---
+++ examples/consumer-metrics/src/logger.ts
@@ -0,0 +1,18 @@
+import { createLogger, format, transports } from "winston"
+
+const logger = createLogger({
+    level: "info",
+    format: format.combine(
+        format.colorize(),
+        format.timestamp(),
+        format.align(),
+        format.splat(),
+        format.printf(({ timestamp, level, message }) => `${ timestamp } - ${ level }: ${ message }`)
+    ),
+    transports: [
+        new transports.Console()
+    ],
+})
+
+export const LOG_SEPARATOR_LINE = "* * * * * * * * * * * * *"
+export { logger }
